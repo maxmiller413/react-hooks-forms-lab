a85df25657291f0dfe1657ee05fe6311ab8cfc3b
Max Miller <maxmiller413@gmail.com>
2021-04-11 19:00:46 -0500

    commit tree 87d7007e5c19d7d6723ed62e489c7aba2a4a7d28

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,9 +1,22 @@
 import React from "react";
 
-function Filter({ onCategoryChange }) {
+function Filter({ onCategoryChange, search, onSearchChange }) {
+  
+  function handleSearchChange(event){
+    return(
+      onSearchChange(event.target.value)
+    )
+  }
+
   return (
     <div className="Filter">
-      <input type="text" name="search" placeholder="Search..." />
+      <input 
+        type="text" 
+        name="search" 
+        placeholder="Search..." 
+        value={search}
+        onChange={handleSearchChange}
+      />
       <select name="filter" onChange={onCategoryChange}>
         <option value="All">Filter by category</option>
         <option value="Produce">Produce</option>
@@ -15,3 +28,13 @@ function Filter({ onCategoryChange }) {
 }
 
 export default Filter;
+
+// 1) Add/set state in Shopping lists
+//    -import React, { useState } from "react"; 
+//    -const [search, setSearch] = useState('')
+// 2) pass state and setState as props to Filter
+//    -set state key as state value
+//    -comment out {setState} for later
+// 3) add state as prop to Filter
+// 4) add value property to input element and assign to {state}
+// 5) add onChange to input element and assign to {handleOnChange}
